6f8412b31264dfa3e943002e63d05c2a5ec32d26
HexagonWin <hexagonwin@archve.ga>
2021-07-03 15:44:22 +0900

    Rename D802 => F320

--- lineage.mk
+++ lineage.mk
@@ -5,12 +5,12 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/lge/d802/d802.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := d802
-PRODUCT_NAME := lineage_d802
+PRODUCT_DEVICE := f320
+PRODUCT_NAME := lineage_f320
 PRODUCT_BRAND := LGE
-PRODUCT_MODEL := LG-D802
+PRODUCT_MODEL := LG-F320
 PRODUCT_MANUFACTURER := lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=lge/g2_open_com/g2:5.0.2/LRX22G/151061918340a:user/release-keys \
-    PRIVATE_BUILD_DESC="g2_open_com-user 5.0.2 LRX22G 151061918340a release-keys"
+    BUILD_FINGERPRINT=lge/g2_kr/g2:5.0.2/LRX22G/151061918340a:user/release-keys \
+    PRIVATE_BUILD_DESC="g2_kr-user 5.0.2 LRX22G 151061918340a release-keys"
